8b88782cfe92c6a943125516b474aca673c6b372
FuzzY <chipercualexandru@gmail.com>
2023-01-23 21:39:59 +0300

    NavMenuHome code refactoring

--- src/components/nav_menu/Home/NavMenuHome.js
+++ src/components/nav_menu/Home/NavMenuHome.js
@@ -2,12 +2,22 @@ import React from 'react';
 import css from './Home.module.css'
 import nav_css from '../Nav.module.css'
 
+
+
+const MainDiv = (props) => {
+    return (
+        <div id="main" className={css.home}>
+            <InnerDiv/>
+        </div>
+    );
+}
+const InnerDiv = (props) => {
+    return <div className={nav_css.nav_txt}>Home</div>
+}
+
+
 const NavMenuHome = () => {
-    return(
-            <div className={css.home}>
-                <a className={nav_css.nav_txt} href='#'>Home</a>
-            </div>
-    )
+    return <MainDiv/>
 }
 
 export default NavMenuHome;
